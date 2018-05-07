7bfd9dc141a03db1e8ae6f1ad8d22072decbfb00
sgardn <sgardn04@gmail.com>
2018-05-07 17:28:49 -0400

    Update react native so the example will work, add TextInput and KeyboardAvoidingView to Login component to reproduce bug

--- Example/components/Login.js
+++ Example/components/Login.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, TextInput, KeyboardAvoidingView} from "react-native";
 import Button from "react-native-button";
 import {Actions} from "react-native-router-flux";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: "#F5FCFF",
   },
@@ -20,6 +20,15 @@ const styles = StyleSheet.create({
     color: "#333333",
     marginBottom: 5,
   },
+  inputWrapper: {
+    flexDirection: 'row',
+    height: 30,
+    borderColor: 'blue',
+    borderWidth: 2
+  },
+  input: {
+    flexGrow: 1,
+  }
 });
 
 export default class extends React.Component {
@@ -36,14 +45,17 @@ export default class extends React.Component {
     const data = this.props.data || 'No Data';
     console.log("Login RENDER");
     return (
-      <View style={[styles.container, this.props.style]}>
+      <KeyboardAvoidingView style={[styles.container, this.props.style]} behavior="padding">
         <Text>Login page 1</Text>
         <Text>Title: {title}</Text>
         <Text>Data: {data}</Text>
         <Button onPress={() => Actions.loginModal2({data: "Custom data2", title: "Custom title2"})}>Login 2</Button>
         <Button onPress={() => Actions.refresh({title: "Changed title", data: "Changed data"})}>Change title</Button>
         <Button onPress={Actions.pop}>Back</Button>
-      </View>
+        <View style={styles.inputWrapper}>
+          <TextInput style={styles.input}/>
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
